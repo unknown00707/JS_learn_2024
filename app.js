877f079f952698d3693ec98a782bb3e927d67cc9
willer <130481969+unknown00707@users.noreply.github.com>
2024-05-27 23:49:39 +0900

    mid
    중간..?까지 왔다..!

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const h1 = document.querySelector('h1');
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
-function clickOn() {}
+const HIDDEN_CLASSSNAME = 'hidden';
+const username_key = 'username';
 
-h1.addEventListener('click', clickOn);
+function onLoginsubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(username_key, username);
+    paintingGreeting(username);
+}
+
+function paintingGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSSNAME);
+}
+
+const checkusername = localStorage.getItem(username_key);
+
+if (checkusername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSSNAME);
+    loginForm.addEventListener('submit', onLoginsubmit);
+} else {
+    paintingGreeting(checkusername);
+}
